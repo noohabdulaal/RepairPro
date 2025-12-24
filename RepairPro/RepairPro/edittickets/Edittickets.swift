2448a30bd231b7e5452e65865b737212635f2d0b
knack133 <knack1639@gmail.com>
2025-12-24 12:22:09 +0300

    edit tickets page

---
+++ RepairPro/RepairPro/edittickets/Edittickets.swift
@@ -0,0 +1,29 @@
+//
+//  Edittickets.swift
+//  RepairPro
+//
+//  Created by BP-36-201-17 on 24/12/2025.
+//
+
+import UIKit
+
+class Edittickets: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
