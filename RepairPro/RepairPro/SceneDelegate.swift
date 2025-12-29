d96dc1da37e16535a9c2806c9d07280b1b70bb05
ec2-user <ec2-user@ip-172-31-34-126.ap-south-1.compute.internal>
2025-12-29 22:03:38 +0000

    added edit technician screen design

--- RepairPro/RepairPro/SceneDelegate.swift
+++ RepairPro/RepairPro/SceneDelegate.swift
@@ -13,8 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     
-    func scene(
-        _ scene: UIScene,
+    func scene(_ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
